29cfc0f2f0c86b0df1867e94d134a9a969a8950f
Anton Krylov <anthony.kryloff@gmail.com>
2017-03-17 17:00:39 +0300

    complete controller example;

--- src/main/java/ru/kpfu/itis/pita/controller/MainPageController.java
+++ src/main/java/ru/kpfu/itis/pita/controller/MainPageController.java
@@ -1,8 +1,13 @@
 package ru.kpfu.itis.pita.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import ru.kpfu.itis.pita.repository.UserRepository;
 
 /**
  * By Anton Krylov (anthony.kryloff@gmail.com)
@@ -12,9 +17,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainPageController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(path = "/hi")
-    @ResponseBody
-    public String doGet() {
-        return "main page body";
+    public ModelAndView doGet(ModelMap modelMap) {
+        modelMap.put("user", userRepository.findOne(1));
+        return new ModelAndView("hi.jsp", modelMap);
     }
 }
