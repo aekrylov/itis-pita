6debc9b9c47be1946363e8ca594317499b3fa4da
Damir <serazetdinow.damir@yandex.ru>
2017-03-30 21:45:30 +0300

    course_list.ftl, ListOfCoursesPageController added

--- src/main/java/ru/kpfu/itis/pita/service/CourseService.java
+++ src/main/java/ru/kpfu/itis/pita/service/CourseService.java
@@ -2,6 +2,8 @@ package ru.kpfu.itis.pita.service;
 
 import ru.kpfu.itis.pita.entity.Course;
 
+import java.util.List;
+
 /**
  * Created by 1 on 29.03.2017.
  */
@@ -10,4 +12,6 @@ public interface CourseService {
     Course create(Course course);
 
     boolean exists(String courseName);
+
+    List<Course> getAll();
 }
